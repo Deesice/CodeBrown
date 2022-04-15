a4d07ddc7f5136a1fb69634a8a0b40dccaf8e35f
Deesice <70575939+Deesice@users.noreply.github.com>
2022-04-15 20:33:17 +0300

    ADs

--- Assets/Scripts/LoadingCurtain.cs
+++ Assets/Scripts/LoadingCurtain.cs
@@ -9,22 +9,24 @@ public class LoadingCurtain : MonoBehaviour
     Vector3 initialPosition;
     static LoadingCurtain instance;
     Image image;
+    float offsetInPixels;
     private void Awake()
     {
         image = GetComponent<Image>();
         image.enabled = true;
         instance = this;
         transform = GetComponent<RectTransform>();
+        offsetInPixels = Screen.width * 3;
     }
     public static void ShowScene()
     {
         instance.transform.position = instance.initialPosition;
-        instance.StartCoroutine(instance.MovingTo(instance.initialPosition + Vector3.left * 2000, 0.5f));
+        instance.StartCoroutine(instance.MovingTo(instance.initialPosition + Vector3.left * instance.offsetInPixels, 0.5f));
         instance.GetComponent<AudioSource>().Play();
     }
     public static void HideScene()
     {
-        instance.transform.position = instance.initialPosition + Vector3.right * 2000;
+        instance.transform.position = instance.initialPosition + Vector3.right * instance.offsetInPixels;
         instance.StartCoroutine(instance.MovingTo(instance.initialPosition, 0.5f));
     }
     void Start()
